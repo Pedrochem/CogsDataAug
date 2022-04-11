d1b363708a47af22b09f79b1e27530a7dcf0f063
PedroChem <pedrochem@hotmail.com>
2022-04-11 10:56:12 +0200

    update after gaussier meeting

--- aug_other.py
+++ aug_other.py
@@ -2,15 +2,16 @@
 import ast
 from collections import defaultdict
 from dis import dis
+import re
 
 
 NOUN = 'noun'
 MAX_PERMS = 3
 
 # train_file = open("Data/train_pos.tsv")
-train_file = open("data/gen.tsv")
+train_file = open("data/dev.tsv")
 
-out_file = open("new_results/up_aug_gen.tsv", "w")
+out_file = open("new_results/up_aug_dev.tsv", "w")
 
 p_file = open('helper/permutation_vocab_src.txt')
 # p_file = open('perms.txt')
@@ -27,21 +28,23 @@ def make_p_dic():
         key = ast.literal_eval(splits[0])
         value_splits = splits[1].split(' ')[1:]
         value_splits[-1] = value_splits[-1].rstrip("\n")
-        p_dic[key] = value_splits
+        p_dic[key] = value_splits[:-1]
     return
 
 
 def modify_output(out,pos,w_class,word,distribution):
-    if distribution == 'primitive\n': return out.replace(word,'?'+w_class+str(pos))
+    if distribution == 'primitive\n': 
+        return re.sub(r'\b%s\b' % word , '?'+w_class, out)
+
     if w_class == 'n':
-        new_string = out.replace(word,'?n'+str(pos)) 
+        new_string = re.sub(r'\b%s\b' % word , '?n', out)
         return new_string
     elif w_class == 'v':        
         split_word = '( x _ '+str(pos)+' ,'
         splits = out.split(' ')
         for i,_ in enumerate(splits):
             if ' '.join(splits[i+3:i+8]) == split_word:
-                splits[i] = '?v'+str(pos)
+                splits[i] = '?v'
         new_out = ' '.join(splits)
         return new_out
     else: 
@@ -52,14 +55,14 @@ def clean_utt(utt,p,flag):
     permuted_utt = ''
     for word in splits:
         if '//' in word or (word == p and flag):
-            if '//' in word:
-                word=word[:-3]
+            # if '//' in word:
+            #     word=word[:-3]
             permuted_utt += word + ' '
     return permuted_utt
 
 def make_new_row(utt,output,word,w_class,pos,p):
         splits = utt.split(' ')
-        splits[pos] = p
+        splits[pos] = p+'//'+w_class
         permuted_utt = (' ').join(splits)
         #permuted_utt = clean_utt(permuted_utt,p,True)
         if output[-1]=='\n': output = output[:-1]
--- aug_train.py
+++ aug_train.py
@@ -2,15 +2,15 @@
 import ast
 from collections import defaultdict
 from dis import dis
-
+import re
 
 NOUN = 'noun'
 MAX_PERMS = 3
 
-train_file = open("data/test.tsv")
+train_file = open("data/train_pos.tsv")
 # train_file = open("data/test_train.tsv")
 
-out_file = open("new_results/up_aug_test.tsv", "w")
+out_file = open("results/aug_train.tsv", "w")
 
 p_file = open('helper/permutation_vocab_src.txt')
 # p_file = open('perms.txt')
@@ -27,21 +27,22 @@ def make_p_dic():
         key = ast.literal_eval(splits[0])
         value_splits = splits[1].split(' ')[1:]
         value_splits[-1] = value_splits[-1].rstrip("\n")
-        p_dic[key] = value_splits
+        p_dic[key] = value_splits[:-1]
     return
 
-
 def modify_output(out,pos,w_class,word,distribution):
-    if distribution == 'primitive\n': return out.replace(word,'?'+w_class+str(pos))
+    if distribution == 'primitive\n': 
+        return re.sub(r'\b%s\b' % word , '?'+w_class, out)
+
     if w_class == 'n':
-        new_string = out.replace(word,'?n'+str(pos)) 
+        new_string = re.sub(r'\b%s\b' % word , '?n', out)
         return new_string
     elif w_class == 'v':        
         split_word = '( x _ '+str(pos)+' ,'
         splits = out.split(' ')
         for i,_ in enumerate(splits):
             if ' '.join(splits[i+3:i+8]) == split_word:
-                splits[i] = '?v'+str(pos)
+                splits[i] = '?v'
         new_out = ' '.join(splits)
         return new_out
     else: 
@@ -52,14 +53,14 @@ def clean_utt(utt,p,flag):
     permuted_utt = ''
     for word in splits:
         if '//' in word or (word == p and flag):
-            if '//' in word:
-                word=word[:-3]
+            #if '//' in word:
+                #word=word[:-3]
             permuted_utt += word + ' '
     return permuted_utt
 
 def make_new_row(utt,output,word,w_class,pos,p):
         splits = utt.split(' ')
-        splits[pos] = p
+        splits[pos] = p+'//'+w_class
         permuted_utt = (' ').join(splits)
         if output[-1]=='\n': output = output[:-1]
         final_row = ('\t').join((permuted_utt.strip(),output.strip()))
